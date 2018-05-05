3002e3edd3fec9dbba9b46878e13e407963c652d
Nick Howlett <nicholashowlett1@gmail.com>
2018-05-05 17:20:45 +1000

    Cleans all quotes data retrieved.
    Cleans data by Removing:
    -Empty quotes.
    -HTML markup from within quote.
    -Whitespace/comma/full-stop from only end of quote.

---
+++ README.md
@@ -0,0 +1,7 @@
+# adani
+Single web page that displays numerous quotes, related to Adani, taken from various news articles. #StopAdani
+
+Application developed in Python. For application's design/development plan please refer to TODO's within adani.py. Code currently: 
+1. Retrieves news articles related to Adani, from the Guardian website.
+2. Compiles all quotes from all retrieved news articles into a 2-Dimensional list.
+3. Cleans all quotes data retrieved.
--- adani.py
+++ adani.py
@@ -2,7 +2,7 @@ import urllib3
 import re
 from bs4 import BeautifulSoup
 
-## find The Guardian news articles that are about Adani
+# finds news articles that are about Adani from the Guardian's website
 searchURL = "https://news.google.com/news/rss/search/section/q/adani+guardian/adani+guardian?hl=en-AU&gl=AU&ned=au"
 http = urllib3.PoolManager()
 searchResults = http.request('GET', searchURL)
@@ -12,38 +12,52 @@ searchResultsListTrim = []
 searchResultsListTrim = searchResultsList[3:] # remove irrelevant data at start of list
 urls = []
 for searchResult in searchResultsListTrim:
-	searchResultSplit= re.split('\/', searchResult) # get domain name
-	searchResultURL = re.split('\"', searchResult) # get full URL
-	matchNews = re.search('news', searchResultSplit[2])
-	matchEncrypted = re.search('encrypted', searchResultURL[0])
-	matchDouble = re.search('\<\/link>', searchResultURL[0])
-	if not matchNews and not matchEncrypted and not matchDouble:
-		searchResultURL = "http" + searchResultURL[0]
-		urls.append(searchResultURL)
+    searchResultSplit= re.split('\/', searchResult) # get domain name
+    searchResultURL = re.split('\"', searchResult) # get full URL
+    matchNews = re.search('news', searchResultSplit[2])
+    matchEncrypted = re.search('encrypted', searchResultURL[0])
+    matchDouble = re.search('\<\/link>', searchResultURL[0])
+    if not matchNews and not matchEncrypted and not matchDouble:
+        searchResultURL = "http" + searchResultURL[0]
+        urls.append(searchResultURL)
 
-## get all quotes from all The Guardian news articles found
+# gets all quotes from all news articles found
 http = urllib3.PoolManager()
-quotesAll = []
+quotesAllRaw = []
 for url in urls:
-	# get text from news website
-	page = http.request('GET', url)
-	page = page.data.decode("utf-8") 
-	soup = BeautifulSoup(page, 'html.parser')
-	text = str(soup.find_all('p'))
-	# find all quotes in text
-	quotesRaw = []
-	textSplitList = re.split('\u201c', text)
-	for textSplit in textSplitList:
-		quoteList = re.split('\u201d', textSplit)
-		quotesRaw.append(quoteList[0])
-	quotes = quotesRaw[1:]
-	quotesAll.append(quotes) #quotesAll is a list of list of quotes
+    # get text from news website
+    page = http.request('GET', url)
+    page = page.data.decode("utf-8") 
+    soup = BeautifulSoup(page, 'html.parser')
+    text = str(soup.find_all('p'))
+    # find all quotes in text
+    quotesRaw = []
+    textSplitList = re.split('\u201c', text)
+    for textSplit in textSplitList:
+        quoteList = re.split('\u201d', textSplit)
+        quotesRaw.append(quoteList[0])
+    quotes = quotesRaw[1:]
+    quotesAllRaw.append(quotes) # 2-D list of list of quotes
 
-# get only the quotes that have 'Adani'/'project'/'it' as part of that quote
+# cleans quotes data by removing empty quotes, HTML markup from within quote, then whitespace/comma/full-stop from only end of quote
+quotesAll = []   
+for elementRaw in quotesAllRaw:
+    element = []
+    if elementRaw:
+        for j in range(len(elementRaw)):
+            elementRaw[j] = re.sub('<.*?>', '', elementRaw[j])
+            elementRaw[j] = elementRaw[j].rstrip()
+            elementRaw[j] = re.sub('[.,]$', '', elementRaw[j])
+            element.append(re.sub('[.,]$', '', elementRaw[j]))
+        quotesAll.append(element)
 
-# combine quotes into single quote string
+# TODO: get only the quotes that have 'Adani'/'project'/'it' as part of that quote. NOTE: this may induce empty list elements in quotesAll
 
-# send quote string to web browser (single page)
+# TODO: randomise quotes order then combine into single quote string. DEV: combine into single 1-D list then random.shuffle(list) twice
+
+# TODO: add news article hyperlinks to each quote within single quote string
+
+# TODO: send quote string as a single page to web browser
 #def application(environ, start_response):
     # status = '200 OK'
     # output = b'output.'
